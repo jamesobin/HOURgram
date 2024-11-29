f97e715aec5e41918025a8bb5318e151a771c026
jamesobin <jamesobin@gmail>
2024-11-29 18:33:42 +0900

    [권오빈]  - H:OURgram 프로젝트 생성

---
+++ src/main/java/com/jamesobin/hourgram/HouRgramApplication.java
@@ -0,0 +1,13 @@
+package com.jamesobin.hourgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouRgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouRgramApplication.class, args);
+	}
+
+}
